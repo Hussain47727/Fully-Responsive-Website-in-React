52bd3ec424348f72703d0c04e7b9e66b6b83d5ae
Hussain <hsrar388@gmail.com>
2024-09-15 21:40:25 -0700

    'react'

--- Fully-Responsive-Site/src/App.jsx
+++ Fully-Responsive-Site/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import Programs from './components/programs/Programs'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Programs />
     </div>
   )
 }
--- Fully-Responsive-Site/src/components/Hero/Hero.jsx
+++ Fully-Responsive-Site/src/components/Hero/Hero.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Hero.css'
+import dark_arrow from '../../assets/dark-arrow.png'
 
 const Hero = () => {
   return (
-    <div className='hero'>
-      
+    <div className='hero container'>
+      <div className="hero-text">
+        <h1>We ensure better education for a better world</h1>
+        <p>Our cutting-edge curriculum is design to empower students with the
+            knowledge, skill, and experienced needed to excel in the dynamic field
+            fo education</p>
+            <button className='btn'>Explore more <img src={dark_arrow} alt="" /></button>
+      </div>
     </div>
   )
 }
