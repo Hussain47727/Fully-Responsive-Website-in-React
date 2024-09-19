a8f1c613d5cd9b66858a193fafd0ee7769b450bb
Hussain <hsrar388@gmail.com>
2024-09-19 06:36:48 -0700

    'slider'

--- Fully-Responsive-Site/src/components/Testimonials/Testimonials.jsx
+++ Fully-Responsive-Site/src/components/Testimonials/Testimonials.jsx
@@ -12,9 +12,74 @@ const Testimonials = () => {
     <div className='testimonials'>
         <img src={next_btn} alt=""  className='next-btn'/>
         <img src={back_btn} alt=""  className='back-btn'/>
-      {/* <div className="slider">
-
-      </div> */}
+      <div className="slider">
+          <ul>
+            <li>
+              <div className="slide">
+                <div className="user-info">
+                  <img src={user_1} alt="" />
+                  <div>
+                    <h3>Waqar Abbas</h3>
+                    <span>Technity, Gilgit</span>
+                  </div>
+                </div>
+                <p>Choosing to pursue my degree at Educity was one
+                  of the best decisions I've ever made. The supportive
+                  community, state-of-the-art facilities, and commitment to 
+                  acadmic excellence have truly exceeded my expectation.
+                </p>
+              </div>
+            </li>
+            <li>
+              <div className="slide">
+                <div className="user-info">
+                  <img src={user_2} alt="" />
+                  <div>
+                    <h3>Waqar Abbas</h3>
+                    <span>Technity, Gilgit</span>
+                  </div>
+                </div>
+                <p>Choosing to pursue my degree at Educity was one
+                  of the best decisions I've ever made. The supportive
+                  community, state-of-the-art facilities, and commitment to 
+                  acadmic excellence have truly exceeded my expectation.
+                </p>
+              </div>
+            </li>
+            <li>
+              <div className="slide">
+                <div className="user-info">
+                  <img src={user_3} alt="" />
+                  <div>
+                    <h3>Waqar Abbas</h3>
+                    <span>Technity, Gilgit</span>
+                  </div>
+                </div>
+                <p>Choosing to pursue my degree at Educity was one
+                  of the best decisions I've ever made. The supportive
+                  community, state-of-the-art facilities, and commitment to 
+                  acadmic excellence have truly exceeded my expectation.
+                </p>
+              </div>
+            </li>
+            <li>
+              <div className="slide">
+                <div className="user-info">
+                  <img src={user_4} alt="" />
+                  <div>
+                    <h3>Waqar Abbas</h3>
+                    <span>Technity, Gilgit</span>
+                  </div>
+                </div>
+                <p>Choosing to pursue my degree at Educity was one
+                  of the best decisions I've ever made. The supportive
+                  community, state-of-the-art facilities, and commitment to 
+                  acadmic excellence have truly exceeded my expectation.
+                </p>
+              </div>
+            </li>
+          </ul>
+      </div>
     </div>
   )
 }
